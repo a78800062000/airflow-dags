6814844d6034cbc3da1b1a88869d58f39904a1e9
a78800062000 <48191668+a78800062000@users.noreply.github.com>
2024-03-17 11:13:54 +0800

    Update simple_dag.py，Update start date

--- simple_dag.py
+++ simple_dag.py
@@ -12,7 +12,7 @@ def my_print_function():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 3, 16),
+    'start_date': datetime(2024, 4, 16),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
